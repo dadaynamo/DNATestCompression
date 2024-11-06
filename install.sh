2be1cb0ed11f199c6792aa5e6a0f543cc70e92a7
dadaynamo <dadaymattia@gmail.com>
2024-11-06 02:00:43 +0100

    End day

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Script per clonare una lista fissa di repository Git
 
-# Lista delle repository da clonare
+# Lista repo da installare
 repos=(
     "https://github.com/dadaynamo/EDS_GEN.git"
     "https://github.com/dadaynamo/DNAStructureInfo.git"
@@ -15,3 +15,37 @@ for repo in "${repos[@]}"; do
     git clone "$repo"
     echo "Clonazione completata per $repo."
 done
+
+# Compilazione delle varie repo
+cd /EDS_GEN
+make
+cd ..
+cd /DNAStructureInfo
+make
+cd ..
+
+
+echo -n "Caricamento: ["
+# Esegui un ciclo per simulare il progresso
+for ((i = 0; i <= 50; i++)); do
+    sleep 0.1  # Ritardo per simulare un'attesa
+    echo -n "#"
+done
+echo "] Completato!"
+
+clear
+
+./EDS_GEN
+
+#If vuoi creare un file raw clicca 1
+    # Se si allora creo file RAW
+    # Comprimo il file in tot modi diversi
+    # Confronto tra file
+
+    # Else creo il file EDS con X parametri
+    # Espansione del file eds in formato Raw
+    # Compressione sequenziale del file originale
+    # Confronto tra file
+
+
+
