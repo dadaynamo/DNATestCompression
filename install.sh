5d78610849ecb55b7d7b00632eb7e0a8b4a55b68
dadaynamo <dadaymattia@gmail.com>
2024-12-12 19:03:55 +0100

    bug fix

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ mkdir -p file_compressed/
 repos=(
     "https://github.com/dadaynamo/EDS_GEN.git"
     "https://github.com/dadaynamo/DNAStructureInfo.git"
-    "https://github.com/felipelouza/gsufsort.git"
 )
 
 # Ciclo su ciascun URL di repository e clonazione
@@ -18,55 +17,4 @@ for repo in "${repos[@]}"; do
     git clone "$repo"
     echo "Clonazione completata per $repo."
 done
-
-# Compilazione delle varie repo
-# Scorri tutte le cartelle nella directory corrente
-for dir in */ ; do
-    # Controlla se è effettivamente una directory
-    if [ -d "$dir" ]; then
-        echo "Entrando nella cartella $dir"
-        cd "$dir"
-        
-        # Esegui 'make' solo se un Makefile è presente
-        if [ -f "Makefile" ] || [ -f "makefile" ]; then
-            echo "Eseguendo 'make' in $dir"
-            make
-        else
-            echo "Nessun Makefile trovato in $dir, saltando."
-        fi
-        
-        # Torna alla cartella principale
-        cd ..
-    fi
-done
-
-# Compilazione edsToRaw
-cd EDS_GEN/edsToRaw
-make
-cd -
-cd EDS_GEN/stringCheck
-make
-cd -
-
-git clone "https://github.com/simongog/sdsl-lite.git"
-cd sddl-lite
-./install.sh ../
-cd ..
-
-echo -n "Caricamento: ["
-# Esegui un ciclo per simulare il progresso
-for ((i = 0; i <= 50; i++)); do
-    sleep 0.1  # Ritardo per simulare un'attesa
-    echo -n "#"
-done
-echo "] Completato!"
-sleep 1
-clear
-echo "\ Ora puoi avviare DNATestCompression.sh per eseguire la procedura /"
-sleep 3
-clear
-
-
-
-
 
